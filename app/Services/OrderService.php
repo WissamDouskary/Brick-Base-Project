dcb38e52619a212d7e32391f9d0eeb379ef38b71
WissamDouskary <douskary.wissam@gmail.com>
2025-04-20 15:29:07 +0100

    create orders service and make create method

---
+++ app/Services/OrderService.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Services;
+
+use App\Repositories\Contracts\OrderRepositoryInterface;
+use Illuminate\Support\Facades\Auth;
+
+
+Class OrderService {
+
+    protected $orderrepository;
+
+    public function __construct(OrderRepositoryInterface $order_repository)
+    {
+        $this->orderrepository = $order_repository;
+    }
+
+    public function create($data){
+        return $this->orderrepository->store($data);
+    }
+
+}
