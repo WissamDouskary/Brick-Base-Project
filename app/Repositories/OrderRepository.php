a113a8e271179fd3ae5e1c2b802afe5e6bba82cb
WissamDouskary <douskary.wissam@gmail.com>
2025-04-21 09:28:49 +0100

    create get and filter client orders methods in rder repository

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -13,4 +13,14 @@ class OrderRepository implements OrderRepositoryInterface
     public function store(array $data){
         return Auth::user()->order()->create($data);
     }
+
+    public function getClientOrders()
+    {
+        return Auth::user()->order()->with('product')->paginate(6);
+    }
+
+    public function filterClientOrders($status)
+    {
+        return Auth::user()->order()->where('status', $status)->paginate(6);
+    }
 }
