875078ffc5c209d3083c9eed3c74a279c9d2eec2
WissamDouskary <douskary.wissam@gmail.com>
2025-04-22 19:42:24 +0100

    add filter methods in user interface

--- app/Repositories/Contracts/UserRepositoryInterface.php
+++ app/Repositories/Contracts/UserRepositoryInterface.php
@@ -10,4 +10,8 @@ interface UserRepositoryInterface
     public function findById($id);
     public function getWorkers();
     public function get3workers($id);
+    public function filterByRoleAndStatus($role_id, $is_active);
+    public function getAllUsers();
+    public function filterByActive($is_active);
+    public function filterByRole($role_id);
 }
