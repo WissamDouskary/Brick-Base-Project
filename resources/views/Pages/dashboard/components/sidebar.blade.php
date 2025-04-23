6b302ce52d201395a2b884830aadc3eb2f2de1e6
WissamDouskary <douskary.wissam@gmail.com>
2025-04-23 16:40:44 +0100

    create route for activities dashboard navbar componant

--- resources/views/Pages/dashboard/components/sidebar.blade.php
+++ resources/views/Pages/dashboard/components/sidebar.blade.php
@@ -26,13 +26,13 @@
             <span class="font-medium">People</span>
         </a>
 
-        <a href="" class="flex items-center px-4 py-3 text-gray-700 rounded-lg hover:bg-gray-100 group">
+        <a href="{{ route('dashboard.activities') }}" class="flex items-center px-4 py-3 rounded-lg {{ request()->routeIs('dashboard.activities') ? 'text-blue-600 bg-blue-50' : 'text-gray-700 hover:bg-gray-100' }} group">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-3" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <rect width="18" height="18" x="3" y="3" rx="2" ry="2"></rect>
                 <line x1="3" x2="21" y1="9" y2="9"></line>
                 <line x1="9" x2="9" y1="21" y2="9"></line>
             </svg>
-            <span class="font-medium">Activities</span>
+            <span class="font-medium">Products</span>
         </a>
 
         <a href="" class="flex items-center px-4 py-3 text-gray-700 rounded-lg hover:bg-gray-100 group">
