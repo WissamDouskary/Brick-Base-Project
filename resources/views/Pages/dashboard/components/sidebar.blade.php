df7a516b72f9502a94c66075a8774ea3aeedc8d5
WissamDouskary <douskary.wissam@gmail.com>
2025-04-24 11:50:14 +0100

    create comment sidebar comments page route

--- resources/views/Pages/dashboard/components/sidebar.blade.php
+++ resources/views/Pages/dashboard/components/sidebar.blade.php
@@ -37,7 +37,7 @@
 
         </a>
 
-        <a href="" class="flex items-center px-4 py-3 text-gray-700 rounded-lg hover:bg-gray-100 group">
+        <a href="{{ route('dashboard.comments') }}" class="flex items-center px-4 py-3 rounded-lg {{ request()->routeIs('dashboard.comments') ? 'text-blue-600 bg-blue-50' : 'text-gray-700 hover:bg-gray-100' }} group">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-3" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
             </svg>
