859b76f2a004f6d69a752137a7dc95f730ba4a27
WissamDouskary <douskary.wissam@gmail.com>
2025-04-24 17:06:05 +0100

    send product with comments count to comments dashboard view

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\Contracts\ProductRepositoryInterface;
 use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
+    protected $productservice;
+
+    public function __construct(ProductRepositoryInterface $product_repository)
+    {
+        $this->productservice = $product_repository;
+    }
+
     public function index(){
-        return view('Pages.dashboard.comments');
+        $products = $this->productservice->getProductsWithComments();
+        return view('Pages.dashboard.comments', compact('products'));
     }
 }
