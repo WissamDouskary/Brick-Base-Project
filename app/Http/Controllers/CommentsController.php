0dab7792f38c454a24a1792c67c58e802ab9b822
WissamDouskary <douskary.wissam@gmail.com>
2025-04-24 18:41:31 +0100

    add workers array to comments controller to show products with comments count

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -3,19 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\Contracts\ProductRepositoryInterface;
+use App\Repositories\Contracts\UserRepositoryInterface;
 use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
     protected $productservice;
+    protected $userservice;
 
-    public function __construct(ProductRepositoryInterface $product_repository)
+    public function __construct(ProductRepositoryInterface $product_repository, UserRepositoryInterface $user_repository)
     {
         $this->productservice = $product_repository;
+        $this->userservice = $user_repository;
     }
 
     public function index(){
         $products = $this->productservice->getProductsWithComments();
-        return view('Pages.dashboard.comments', compact('products'));
+        $workers = $this->userservice->getWorkers();
+
+        return view('Pages.dashboard.comments', compact('products', 'workers'));
     }
 }
