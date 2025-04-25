0b18b8d6bcf0dc71b080600fef000b3f9dcf44e4
WissamDouskary <douskary.wissam@gmail.com>
2025-04-25 12:04:34 +0100

    add validation of quantity to be max is 20

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -19,7 +19,7 @@ class OrderController extends Controller
 
     public function store(Request $request){
         $request->validate([
-            'quantity' => 'gt:0|required|numeric',
+            'quantity' => 'gt:0|required|numeric|max:20',
             'price' => 'required'
         ]);
 
