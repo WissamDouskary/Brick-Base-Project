9e7175dde64cf18282c845ac2f637f9814718e12
WissamDouskary <douskary.wissam@gmail.com>
2025-04-25 12:04:20 +0100

    create get workers and products comments to dashboard admin

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\Contracts\ProductRepositoryInterface;
+use App\Repositories\Contracts\ReviewsRepositoryInterface;
 use App\Repositories\Contracts\UserRepositoryInterface;
 use Illuminate\Http\Request;
 
@@ -10,17 +11,21 @@ class CommentsController extends Controller
 {
     protected $productservice;
     protected $userservice;
+    protected $reviewsservice;
 
-    public function __construct(ProductRepositoryInterface $product_repository, UserRepositoryInterface $user_repository)
+    public function __construct(ProductRepositoryInterface $product_repository, UserRepositoryInterface $user_repository, ReviewsRepositoryInterface $reviews_repository)
     {
         $this->productservice = $product_repository;
         $this->userservice = $user_repository;
+        $this->reviewsservice = $reviews_repository;
     }
 
     public function index(){
         $products = $this->productservice->getProductsWithComments();
         $workers = $this->userservice->getWorkers();
+        $productComments = $this->reviewsservice->getProductsReviews(); 
+        $workerComments = $this->reviewsservice->getWorkersReviews();
 
-        return view('Pages.dashboard.comments', compact('products', 'workers'));
+        return view('Pages.dashboard.comments', compact('products', 'workers', 'productComments', 'workerComments'));
     }
 }
